d399d0f53ae6c8e4af720317084fb4b06f105ae5
Jordyn Mancini <jordyn.n.mancini@gmail.com>
2021-04-23 14:48:24 -0400

    filled in missing code for server.js / bug fixes on connection.js and models/index

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false, 
   },
-  as: 'product_tag'
+  as: 'products_tags'
 }); 
 
 // Tags belongToMany Products (through ProductTag)
@@ -30,7 +30,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: 'product_tag'
+  as: 'tags_products'
 }); 
 
 module.exports = {
